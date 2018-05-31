5908396231d1489c37c245b5ae5b83cbdc3ab763
Thiago Mendes <thiago@posteo.de>
2018-05-31 11:21:01 -0300

    donation: fragment layout and auto copy

--- app/src/main/java/com/tmendes/birthdaydroid/fragments/DonationFragment.java
+++ app/src/main/java/com/tmendes/birthdaydroid/fragments/DonationFragment.java
@@ -17,6 +17,7 @@
 
 package com.tmendes.birthdaydroid.fragments;
 
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,105 +25,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tmendes.birthdaydroid.R;
 
+import static android.content.Context.CLIPBOARD_SERVICE;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DonationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DonationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DonationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String BITCOIN_ADDR = "12mgStuib7nGkqzvKjMqc4LQwNFkL1Q7m7";
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public DonationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DonationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DonationFragment newInstance(String param1, String param2) {
-        DonationFragment fragment = new DonationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donation, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_donation, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        TextView bitcoin = v.findViewById(R.id.tvBitcoin);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        bitcoin.setText(BITCOIN_ADDR);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+        bitcoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getContext().
+                        getSystemService(CLIPBOARD_SERVICE);
+                final android.content.ClipData clipData = android.content.ClipData
+                        .newPlainText(BITCOIN_ADDR, BITCOIN_ADDR);
+                clipboard.setPrimaryClip(clipData);
+                Toast.makeText(getContext(), getContext().getResources().
+                        getString(R.string.copy_to_clipboard), Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return v;
     }
 }
