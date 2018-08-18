86acc61c29e4b8c2f6b000dd54d86598a482eee0
revuwa <replicant@posteo.mx>
2018-08-18 22:14:44 +0000

    Setting [DiA] default value to 1

--- app/src/main/java/com/tmendes/birthdaydroid/NumberPickerPreference.java
+++ app/src/main/java/com/tmendes/birthdaydroid/NumberPickerPreference.java
@@ -26,7 +26,7 @@ public class NumberPickerPreference extends DialogPreference {
     public static final boolean WRAP_SELECTOR_WHEEL = true;
 
     private NumberPicker picker;
-    private int value;
+    private int value = 1;
 
     public NumberPickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
