58c8b161810624d51fab50c5fe31b601ca5302e2
Bastian Wilhelm <bw-dev@mailbox.org>
2020-06-07 12:52:03 +0200

    remove unnecessary test

--- app/src/test/java/com/tmendes/birthdaydroid/contact/ContactTest.java
+++ app/src/test/java/com/tmendes/birthdaydroid/contact/ContactTest.java
@@ -3,31 +3,7 @@ package com.tmendes.birthdaydroid.contact;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
-
 public class ContactTest {
-    @Test
-    public void testHasBirthdayToday() {
-        Contact contact = spy(new Contact());
-
-        doReturn(0).when(contact).getDaysUntilNextBirthday();
-        doReturn(false).when(contact).isBornInFuture();
-        Assert.assertTrue(contact.isBirthdayToday());
-
-        doReturn(1).when(contact).getDaysUntilNextBirthday();
-        doReturn(false).when(contact).isBornInFuture();
-        Assert.assertFalse(contact.isBirthdayToday());
-
-        doReturn(0).when(contact).getDaysUntilNextBirthday();
-        doReturn(true).when(contact).isBornInFuture();
-        Assert.assertFalse(contact.isBirthdayToday());
-
-        doReturn(0).when(contact).getDaysUntilNextBirthday();
-        doReturn(true).when(contact).isBornInFuture();
-        Assert.assertFalse(contact.isBirthdayToday());
-    }
-
     @Test
     public void testFavoriteAndIgnoreSwitch() {
         Contact contact = new Contact();
